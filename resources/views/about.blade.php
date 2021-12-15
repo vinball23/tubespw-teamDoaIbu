77e823173481e5b91e930724b6448967e4e1639c
vinball23 <63390247+vinball23@users.noreply.github.com>
2021-12-16 00:25:59 +0700

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,9 +6,8 @@
             Hi, Welcome to ... 
         </h1>
         <p>
-            Hallo , apa kabar hari ini ? Semoga selalu sehat dalam perlindungan-Nya.
-                
-            Website ... ini kami buat bertujuan untuk memberikan referensi resep masakan mulai dari nusantara sampai manca negara.
+            FoodStation is a website that contains collection of recipes from various parts of the world. 
+            We created this website with the intention of bringing fresh and flavorful recipes right to your palm
         </p>
         <p>
             Harapan kami, dengan adanya website ini bisa membantu anda mencari lokasi wisata yang ingin anda kunjungi bersama keluarga atau orang-orang tercinta.
