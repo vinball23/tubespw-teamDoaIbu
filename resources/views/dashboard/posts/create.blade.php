e6394dd9967a12e6a9a044b1e651f51bbeb7a6f5
m_rizkyprtm <m.rizkypratama319@gmail.com>
2021-12-16 22:18:42 +0700

    fix error di dashboard-posts

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -11,59 +11,64 @@
         <div class="mb-3">
           <label for="title" class="form-label">Title</label>
           <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title') }}">
-            @error('title')
-              <div class="invalid-feedback">
-                {{ $message }}
-              </div>
-            @enderror
+          @error('title')
+            <div class="invalid-feedback">
+              {{ $message }}
+            </div>
+          @enderror
         </div>
+
         <div class="mb-3">
           <label for="slug" class="form-label">Slug</label>
           <input type="text" class="form-control  @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug') }}">
-            @error('slug')
-                <div class="invalid-feedback">
-                  {{ $message }}
-                </div>
-            @enderror
+          @error('slug')
+            <div class="invalid-feedback">
+              {{ $message }}
+            </div>
+          @enderror
         </div>
+
         <div class="mb-3">
           <label for="country" class="form-label">Country</label>
           <select class="form-select" name="country_id">
             @foreach ($countries as $country)
-            @if (old('country_id') == $country->id)
-              <option value="{{ $country->id }}" selected>{{ $country->name }}</option>
-            @else
-              <option value="{{ $country->id }}">{{ $country->name }}</option>
-            @endif
+              @if (old('country_id') == $country->id)
+                <option value="{{ $country->id }}" selected>{{ $country->name }}</option>
+              @else
+                <option value="{{ $country->id }}">{{ $country->name }}</option>
+              @endif
             @endforeach
           </select>
         </div>
+
         <div class="mb-3">
           <label for="category" class="form-label">Category</label>
           <select class="form-select" name="category_id">
             @foreach ($categories as $category)
-            @if (old('category_id') == $category->id)
-              <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
-            @else
-              <option value="{{ $category->id }}">{{ $category->name }}</option>
-            @endif
+              @if (old('category_id') == $category->id)
+                <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
+              @else
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+              @endif
             @endforeach
           </select>
         </div>
+
         <div class="mb-3">
           <label for="image" class="form-label">Post Image</label>
           <img class="img-preview img-fluid mb-3 col-sm-5">
           <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
           @error('image')
-              <div class="invalid-feedback">
-                {{ $message }}
-              </div>
+            <div class="invalid-feedback">
+              {{ $message }}
+            </div>
           @enderror
         </div>
+        
         <div class="mb-3">
           <label for="body" class="form-label">Body</label>
           @error('body')
-              <p class="text-danger">{{ $message }}</p>
+            <p class="text-danger">{{ $message }}</p>
           @enderror
           <input id="body" type="hidden" name="body" value="{{ old('body') }}">
           <trix-editor input="body"></trix-editor>
@@ -82,23 +87,23 @@
       .then(data => slug.value = data.slug)
   });
 
-document.addEventListener('trix-file-accept', function(e) {
-  e.preventDefault();
-})
+  document.addEventListener('trix-file-accept', function(e) {
+    e.preventDefault();
+  })
 
-function previewImage() { 
-const image = document.querySelector('#image');
-const imgPreview = document.querySelector('.img-preview');
+  function previewImage() { 
+  const image = document.querySelector('#image');
+  const imgPreview = document.querySelector('.img-preview');
 
-imgPreview.style.display = 'block';
+  imgPreview.style.display = 'block';
 
-const oFReader = new FileReader();
-oFReader.readAsDataURL(image.files[0]);
+  const oFReader = new FileReader();
+  oFReader.readAsDataURL(image.files[0]);
 
-oFReader.onload = function (oFREvent) {
-    imgPreview.src = oFREvent.target.result;
+  oFReader.onload = function (oFREvent) {
+      imgPreview.src = oFREvent.target.result;
+    }
   }
-}
 
 </script>
     
